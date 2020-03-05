89b488161f8dbd6d84b8bbdabae65e39849500aa
jacekku <kecajwy@gmail.com>
2020-03-05 13:03:44 +0100

    adding prepopulation to storage

--- src/main/java/pl/jwoj/storage/InMemoryBookshelfStorage.java
+++ src/main/java/pl/jwoj/storage/InMemoryBookshelfStorage.java
@@ -1,11 +1,17 @@
 package pl.jwoj.storage;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import pl.jwoj.config.exceptions.BookNotFoundException;
 import pl.jwoj.domain.Book;
 
+import javax.swing.text.html.Option;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
@@ -15,6 +21,18 @@ public class InMemoryBookshelfStorage implements BookshelfStorage {
 
 	public InMemoryBookshelfStorage() {
 		this.books = new ConcurrentHashMap<>();
+		prepopulateStorage();
+	}
+	private void prepopulateStorage() {
+		ObjectMapper om = new ObjectMapper();
+		URL url = InMemoryBookshelfStorage.class.getResource("/static/tempBooks.json");
+		List<Book> values = null;
+		try {
+			 values = om.readValue(url, new TypeReference<List<Book>>() {});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		values.forEach(this::createBook);
 	}
 
 	@Override
