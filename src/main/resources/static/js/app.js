30a5da3d33767df755716151da54280fcb7f2d32
csterkowiec <CaStet97@gmail.com>
2020-03-08 13:25:45 +0100

    Putting storage class into another file

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -176,22 +176,7 @@ class UI{
         return buttonsDOM.find(button => button.dataset.id === id);
     }
 }
-//local storage
-class Storage{
-    static saveProducts(products){
-        localStorage.setItem("products", JSON.stringify(products));
-    }
-    static getProduct(id){
-        let products = JSON.parse(localStorage.getItem('products'));
-        return products.find(product => String(product.id) === id);
-    }
-    static saveCart(cart){
-        localStorage.setItem("cart", JSON.stringify(cart));
-    }
-    static getCart(){
-        return localStorage.getItem('cart')?JSON.parse(localStorage.getItem('cart')):[];
-    }
-}
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
 const ui = new UI();
---
+++ src/main/resources/static/js/storage.js
@@ -0,0 +1,16 @@
+//local storage
+class Storage{
+    static saveProducts(products){
+        localStorage.setItem("products", JSON.stringify(products));
+    }
+    static getProduct(id){
+        let products = JSON.parse(localStorage.getItem('products'));
+        return products.find(product => String(product.id) === id);
+    }
+    static saveCart(cart){
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+    static getCart(){
+        return localStorage.getItem('cart')?JSON.parse(localStorage.getItem('cart')):[];
+    }
+}
